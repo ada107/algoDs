1dfbfaf52981ba2d59f548b9ad5ac85500a9ca1a
ada107 <44057255+ada107@users.noreply.github.com>
2020-05-09 16:32:39 +0530

    Create Coin Sum Infinite.java

---
+++ DP6/Coin Sum Infinite.java
@@ -0,0 +1,17 @@
+public class Solution {
+    public int coinchange2(int[] A, int B) {
+        int dp1[] = new int[B+1];
+        dp1[0] = 1;
+        for(int i=0;i<A.length;i++){
+            int dp2[] = new int[B+1];
+            for(int j=0;j<=B;j++){
+                if(j>=A[i]){
+                    dp2[j] = dp1[j] + dp2[j-A[i]];
+                }
+                else dp2[j] = dp1[j];
+            }
+            dp1 = dp2;
+        }
+        return dp1[B];
+    }
+}
